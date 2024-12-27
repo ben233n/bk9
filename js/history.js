32846d48c0ba841ff77e53b61e917ddb7d05f45b
ben233n <154246877+ben233n@users.noreply.github.com>
2024-12-27 11:55:04 +0800

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     const isSmallScreen = window.innerWidth < 1200; // 檢查螢幕是否小於700
-    
-    if (isSmallScreen) {
+    const isSmallScreenb = window.innerWidth > 1600;
+    if (isSmallScreen || isSmallScreenb ) {
       // 刪除 .history div 和其內部內容
       $('.history').remove();
   
@@ -14,7 +14,36 @@ $(document).ready(function() {
 
   gsap.registerPlugin(ScrollTrigger);
 
-if ($(window).width() >= 1200){
+if($(window).width() >= 1600){
+    $(".big_title_box").each(function(index, element) {
+        // 對每個元素應用 GSAP 動畫
+        gsap.fromTo(
+            element,
+            {
+                opacity: 1,
+                y: 0 // 初始位置：向下偏移 50px
+            },
+            {
+                opacity: 0,
+                y: 0, // 最終位置：回到原點
+                duration: 0.5,
+                ease: "ease.out",
+                scrollTrigger: {
+                    trigger: element, // 設定觸發的目標元素
+                    start: "top-=500 -100%", // 當元素頂部到達螢幕的 80% 高度時觸發
+                    end: "top-=500 -200%", // 當元素頂部到達螢幕的 30% 高度時結束
+                    toggleActions: "play reverse play reverse", // 定義滾動進出行為
+                    // markers: true,       // 顯示觸發點的標記，方便調試
+                    scrub: true, 
+                }
+            }
+        );
+    });
+
+
+
+}
+else if ($(window).width() >= 1200){
     $(".big_title_box").each(function(index, element) {
         // 對每個元素應用 GSAP 動畫
         gsap.fromTo(
